59e87df2dc2b1b904452d9a38d9604c1690c6c2f
sajid365-sr <sajidsorker86@gmail.com>
2022-11-30 02:51:08 +0600

    login form complete

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { FaFacebook, FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
+
+
+  const handleLogin = (data) =>{
+
+    const {email, password} = data;
+
+  }
 
   return (
     <div className="bg-gradient-to-bl from-[#270a6b] to-secondary flex justify-center items-center h-[90vh] rounded-2xl">
       <div className="w-2/6 bg-info py-10 rounded-lg bg-opacity-30">
         <h2 className="text-center mb-10 text-5xl text-gray-900">Login</h2>
-        <form className="px-6 flex flex-col gap-4" action="">
-          <div>
-          <label className="text-gray-300" htmlFor="name">Enter Your Name</label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Type here"
-            className="input text-gray-700 mt-1 font-medium focus:outline-secondary w-full"
-            {...register('name')}
-          />
-          </div>
+        <form onSubmit={handleSubmit(handleLogin)} className="px-6 flex flex-col gap-4" action="">
           <div>
           <label className="text-gray-300" htmlFor="name">Enter Your Email</label>
           <input
@@ -38,7 +37,17 @@ const Login = () => {
             className="input text-gray-700 mt-1 font-medium focus:outline-secondary w-full"
             {...register('password')}
           />
+          <label role='button' className="text-gray-300 hover:text-info underline text-sm" htmlFor="name">Forgot Your password?</label>
           </div>
+          <div className="divider">OR</div>
+          <div className="flex justify-center gap-5">
+              <FaFacebook className="text-3xl hover:text-info" role='button'
+              />
+              <FaGoogle className="text-3xl hover:text-info" role='button'
+              />
+          </div>
+          <p className="text-sm mt-3">Don't have any account? Please <Link className="underline text-info" to='/signup'>register</Link> here.</p>
+            <input className="btn text-lg tracking-widest" type="submit" value='Login' />
         </form>
       </div>
     </div>
