ecb647f30d1607755018f3517e5a9be80a2fe8ac
sajid365-sr <sajidsorker86@gmail.com>
2022-12-07 20:20:29 +0600

    add a product route complete with full functionality

--- src/Pages/Dashboard/AddAProduct/AddAProduct.js
+++ src/Pages/Dashboard/AddAProduct/AddAProduct.js
@@ -1,3 +1,5 @@
+import { async } from "@firebase/util";
+import { useQuery } from "@tanstack/react-query";
 import { Rating } from "flowbite-react";
 import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -5,51 +7,53 @@ import toast from "react-hot-toast";
 import { HiArrowNarrowDown } from "react-icons/hi";
 import { UserContext } from "../../../Contexts/AuthContext/AuthContext";
 
+
 const AddAProduct = () => {
   const [category, setCategory] = useState(false);
-    const {user} = useContext(UserContext);
-
-    const ImageHostKey = process.env.REACT_APP_imgUploadKey;
-    
+  const { user } = useContext(UserContext);
+  const { register, handleSubmit } = useForm();
+  const ImageHostKey = process.env.REACT_APP_imgUploadKey;
+   
+    // const { data :categoryName  = [], refetch} = useQuery({
+    //     queryKey:['categoryName'],
+    //     queryFn: async() =>{
+    //         const res = await fetch('http://localhost:5000/getName');
+    //         const data = await res.json();
+    //         return data;
+    //     }
+    // })
+    // console.log(categoryName)
 
   const handleCategory = (event) => {
     const value = event.target.value;
     if (value === "Others") {
       setCategory(true);
     }
+    
   };
 
-  const handleEmail =(event) =>{
-      event.target.value = user?.email;
-      
-  }
+  const handleEmail = (event) => {
+    event.target.value = user?.email;
+  };
 
-  const handleName = (event) =>{
-    event.target.value = user?.displayName? user.displayName : '';
-  }
+  const handleName = (event) => {
+    event.target.value = user?.displayName ? user.displayName : "";
+  };
 
-  const { register,handleSubmit } = useForm();
+  const options = [
+      { value: "Science Fiction"},{ value: "History and Tradition"},{ value: "Fiction"},{ value: "Islamic"},{ value: "Admission"},{ value: "Travel"},{ value: "Philosophy and Philosopher"},{ value: "Biographies, Memories & Interviews"},{ value: "Politics"},{ value: "Story"},{ value: "Business, Investing & Economics"},{ value: "Books of Drama"},{ value: "Others"}
+  ];
 
-  const handleFormSubmit = (book) =>{
-    // const { author,
-    //     category,
-    //     description,
-    //     email,
-    //     location,
-    //     originalPrice,
-    //     phone,
-    //     publishYear,
-    //     resalePrice,
-    //     sellerName,
-    //     title,
-    //     ratings,
-    //     reviews
-    // } = data;
 
-      const img = book.picture[0];
+
+  const handleFormSubmit = (book, event) => {
+    
+
+    const img = book.picture[0];
     const formData = new FormData();
-    formData.append('image', img);
-
+    formData.append("image", img);
+    
+    
 
     fetch(`https://api.imgbb.com/1/upload?key=${ImageHostKey}`,{
         method:'POST',
@@ -61,6 +65,8 @@ const AddAProduct = () => {
             book.sellerVerified = false;
             book.picture = imgData.data.url;
             
+
+            
             fetch('http://localhost:5000/books',{
                 method:'post',
                 headers:{
@@ -71,22 +77,24 @@ const AddAProduct = () => {
             .then(res => res.json())
             .then(data =>{
                 if(data.acknowledged){
-                    toast.success('Items added successfully')
+                    toast.success('Items added successfully');
+                    event.target.reset();
                 }
                 console.log(data)
             })
 
-           
         }
     })
-  }
+  };
 
   return (
     <div className="max-w-screen-lg mx-auto my-[7%] pt-10 pb-20 px-16 rounded-lg bg-zinc-800">
       <h1 className="text-white text-center text-4xl">Add a new book</h1>
-      <p className="text-gray-400 text-sm mb-8  text-center">Note: <span className="text-red-500 ml-1 text-xl">*</span> mark fields are mandatory</p>
+      <p className="text-gray-400 text-sm mb-8  text-center">
+        Note: <span className="text-red-500 ml-1 text-xl">*</span> mark fields
+        are mandatory
+      </p>
       <form onSubmit={handleSubmit(handleFormSubmit)}>
-
         {/*==================================== Book Title ============================================ */}
 
         <div className="relative z-0 mb-6 w-full group">
@@ -96,11 +104,7 @@ const AddAProduct = () => {
             {...register("title", { required: "Title is required" })}
           />
           <label className="peer-focus:font-medium absolute text-base text-gray-200  duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">
-             
-
-            Book Title 
-            (<span className="text-red-500 text-xl">*</span>)
-              
+            Book Title (<span className="text-red-500 text-xl">*</span>)
           </label>
         </div>
 
@@ -108,7 +112,8 @@ const AddAProduct = () => {
 
         <div className="flex flex-col lg:flex-row items-center md:gap-6 mb-6">
           <div className="relative z-0 mb-6 lg:w-3/4 w-full group">
-            <span className="text-lg mb-2 text-gray-300">Photo</span>(<span className="text-red-500 text-xl">*</span>)
+            <span className="text-lg mb-2 text-gray-300">Photo</span>(
+            <span className="text-red-500 text-xl">*</span>)
             <label
               htmlFor="dropzone-file"
               className="flex flex-col items-center justify-center w-full h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointe  hover:bg-zinc-700"
@@ -135,8 +140,11 @@ const AddAProduct = () => {
                   </span>
                 </p>
               </div>
-              <input id="dropzone-file" type="file" className="hidden"
-              {...register("picture", { required: "Picture is required" })}
+              <input
+                id="dropzone-file"
+                type="file"
+                className="hidden"
+                {...register("picture", { required: "Picture is required" })}
               />
             </label>
           </div>
@@ -176,8 +184,7 @@ const AddAProduct = () => {
               {...register("author", { required: "Author name required" })}
             />
             <label className="peer-focus:font-medium absolute text-lg text-gray-200  duration-300 transform -translate-y-6 scale-75 top-2 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-12">
-              Author
-              (<span className="text-red-500 text-2xl">*</span>)
+              Author (<span className="text-red-500 text-2xl">*</span>)
             </label>
           </div>
           <div className="relative z-0 mb-6 w-full group">
@@ -188,12 +195,14 @@ const AddAProduct = () => {
                     <input
                       type="text"
                       className="block pt-3 px-5 w-full text-base text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#155c72] focus:border-b-4 peer"
-                      {...register("category", { required: "Category is required" })}
+                      {...register("category", {
+                        required: "Category is required",
+                      })}
                     />
 
                     <label className="peer-focus:font-medium absolute text-xl text-gray-200  duration-300 transform -translate-y-5 scale-75 top-3 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:text-sm peer-focus:-translate-y-8">
-                      Category Name 
-                      (<span className="text-red-500 text-xl">*</span>)
+                      Category Name (
+                      <span className="text-red-500 text-xl">*</span>)
                     </label>
                   </div>
                   <p
@@ -207,63 +216,19 @@ const AddAProduct = () => {
               </div>
             ) : (
               <select
-                onChange={handleCategory}
+                onClick={handleCategory}
                 className="block py-2.5 px-0 w-full text-sm text-gray-200 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer"
                 {...register("category", { required: "Category is required" })}
               >
-                <option defaultValue='Choose Category'>
-                  Choose Category '*'
-                  
-                </option>
-                <option className="text-gray-600" value="Science Fiction">
-                  Science Fiction
-                </option>
-                <option className="text-gray-600" value="History and Tradition">
-                  History and Tradition
-                </option>
-                <option className="text-gray-600" value="Fiction">
-                  Fiction
-                </option>
-                <option className="text-gray-600" value="Islamic">
-                  Islamic
-                </option>
-                <option className="text-gray-600" value="Admission">
-                  Admission
-                </option>
-                <option className="text-gray-600" value="Travel">
-                  Travel
-                </option>
-                <option
-                  className="text-gray-600"
-                  value="Philosophy and Philosopher"
-                >
-                  Philosophy and Philosopher
-                </option>
-                <option
-                  className="text-gray-600"
-                  value="Biographies, Memories & Interviews"
-                >
-                  Biographies, Memories & Interviews
-                </option>
-                <option className="text-gray-600" value="Politics">
-                  Politics
-                </option>
-                <option className="text-gray-600" value="Story">
-                  Story
-                </option>
-                <option
-                  className="text-gray-600"
-                  value="Business, Investing & Economics"
-                >
-                  Business, Investing & Economics
-                </option>
-                <option className="text-gray-600" value="Books of Drama">
-                  Books of Drama
-                </option>
-                <option className="text-gray-600" value="Others">
-                  Others
-                </option>
+                  <option className="text-gray-600">Choose a category</option>
+                  {
+                      options.map((option,i) => <option key={i}
+                         className="text-gray-600" value={option.value === 'Others' ? 'Others':`${option.value}`}>{option.value}</option>
+                         
+                         )
+                  }
               </select>
+              
             )}
           </div>
         </div>
@@ -298,85 +263,86 @@ const AddAProduct = () => {
           </div>
         </div>
 
-{/*================================== Seller Name and Location================================= */}
+        {/*================================== Seller Name and Location================================= */}
 
         <div className="grid md:grid-cols-2 md:gap-6 mb-6">
           <div className="relative z-0 mb-6 w-full group">
             <input
-            onClick={ user && handleName}
-            
+              onClick={user && handleName}
               type="text"
               className="block pt-3 px-5 w-full text-base text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#155c72] focus:border-b-4 peer"
-              {...register("sellerName", {required:'Provide seller name'})}
+              {...register("sellerName", { required: "Provide seller name" })}
             />
             <label className="peer-focus:font-medium absolute text-base text-gray-200  duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">
-              Seller Name
-              (<span className="text-red-500 text-xl">*</span>)
+              Seller Name (<span className="text-red-500 text-xl">*</span>)
             </label>
           </div>
           <div className="relative z-0 mb-6 w-full group">
             <input
               type="text"
               className="block pt-3 px-5 w-full text-base text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#155c72] focus:border-b-4 peer"
-              {...register("location", {required:'Provide location'})}
+              {...register("location", { required: "Provide location" })}
             />
             <label className="peer-focus:font-medium absolute text-base text-gray-200  duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">
-              Location
-              (<span className="text-red-500 text-xl">*</span>)
+              Location (<span className="text-red-500 text-xl">*</span>)
             </label>
           </div>
         </div>
 
-{/*==================================== Phone & Email ========================================== */}
+        {/*==================================== Phone & Email ========================================== */}
 
-<div className="grid md:grid-cols-2  md:gap-6 mb-6">
+        <div className="grid md:grid-cols-2  md:gap-6 mb-6">
           <div className="relative z-0 mb-6 w-full group">
             <input
               type="number"
               className="block pt-3 px-5 w-full text-base text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#155c72] focus:border-b-4 peer"
-              {...register("phone", {required:'Phone number is required'})}
+              {...register("phone", { required: "Phone number is required" })}
             />
             <label className="peer-focus:font-medium absolute text-base text-gray-200  duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">
-              Phone
-              (<span className="text-red-500 text-xl">*</span>)
+              Phone (<span className="text-red-500 text-xl">*</span>)
             </label>
           </div>
           <div className="relative z-0 mb-6 w-full group">
             <input
-            onClick={ user && handleEmail}
-            readOnly
+              onClick={user && handleEmail}
+              readOnly
               type="email"
               className="block pt-3 px-5 w-full text-base text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#155c72] focus:border-b-4 peer"
-              {...register("email", {required:'Email is required'})}
+              {...register("email", { required: "Email is required" })}
             />
-            <label  className="peer-focus:font-medium absolute text-base text-gray-200  duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">
-              Email
-              (<span className="text-red-500 text-xl">*</span>)
+            <label className="peer-focus:font-medium absolute text-base text-gray-200  duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">
+              Email (<span className="text-red-500 text-xl">*</span>)
             </label>
           </div>
         </div>
 
-{/*==================================== Price ========================================== */}
+        {/*==================================== Price ========================================== */}
 
         <div className="grid md:grid-cols-2 md:gap-6 mb-6">
           <div className="relative z-0 mb-6 w-full group">
             <input
               type="number"
               className="block pt-3 px-5 w-full text-base text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#155c72] focus:border-b-4 peer"
-              {...register("originalPrice", {required:'Enter original price value'})}
+              {...register("originalPrice", {
+                required: "Enter original price value",
+              })}
             />
             <label className="peer-focus:font-medium absolute text-base text-gray-200  duration-300 transform -translate-y-6 scale-75 top-0 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">
-              Original Price <span className="text-2xl">(&#2547;)</span><span className="text-red-500 ml-1 text-xl">*</span>
+              Original Price <span className="text-2xl">(&#2547;)</span>
+              <span className="text-red-500 ml-1 text-xl">*</span>
             </label>
           </div>
           <div className="relative z-0 mb-6 w-full group">
             <input
               type="number"
               className="block pt-3 px-5 w-full text-base text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#155c72] focus:border-b-4 peer"
-              {...register("resalePrice", {required:'Enter resale price value'})}
+              {...register("resalePrice", {
+                required: "Enter resale price value",
+              })}
             />
             <label className="peer-focus:font-medium absolute text-base text-gray-200  duration-300 transform -translate-y-6 scale-75 top-0 -z-10 origin-[0] peer-focus:left-2 peer-focus:text-orange-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">
-              Resale Price <span className="text-2xl">(&#2547;)</span><span className="text-red-500 ml-1 text-xl">*</span>
+              Resale Price <span className="text-2xl">(&#2547;)</span>
+              <span className="text-red-500 ml-1 text-xl">*</span>
             </label>
           </div>
         </div>
